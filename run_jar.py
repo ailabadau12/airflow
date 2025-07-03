b4ac78830353306986664dd42b70d093fb7bdefe
ailabadau12 <135602111+ailabadau12@users.noreply.github.com>
2025-07-03 14:04:37 +0700

    Update run_jar.py
    dfdf

--- run_jar.py
+++ run_jar.py
@@ -9,26 +9,6 @@ import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-class CustomKubernetesPodOperator(KubernetesPodOperator):
-    def on_kill(self):
-        try:
-            config.load_incluster_config()  # Nếu chạy trong cluster
-
-            api_instance = client.CoreV1Api()
-
-            pod_name = self.name
-            namespace = self.namespace or "default"
-
-            api_instance.delete_namespaced_pod(
-                name=pod_name,
-                namespace=namespace,
-                body=client.V1DeleteOptions(grace_period_seconds=0),
-            )
-            logging.info(f"✅ Đã xoá pod: {pod_name} trong namespace: {namespace}")
-        except Exception as e:
-            logging.warning(f"⚠️ Lỗi khi xoá pod: {e}")
-
-
 #user_defined_schedule = Variable.get("dag_schedule", default_var="2 * * * *")     
 with DAG(
     dag_id='run_java_2',
@@ -38,7 +18,7 @@ with DAG(
     max_active_runs=1,  # Ensures that only one DAG run is allowed to be active at a time; next runs are skipped or queued
     tags=['java', 'kubernetes'],
 ) as dag:    
-     run_jar_task = CustomKubernetesPodOperator(
+     run_jar_task = KubernetesPodOperator(
         task_id='marcus-task-2',
         name='run-java-2',
         namespace='default',  # 
@@ -49,7 +29,7 @@ with DAG(
         image_pull_policy="Always",
 
         get_logs=True,
-        is_delete_operator_pod=True,
+        on_finish_action="delete_pod"
         in_cluster=True,
         startup_timeout_seconds=300,
         execution_timeout=timedelta(minutes=3),
